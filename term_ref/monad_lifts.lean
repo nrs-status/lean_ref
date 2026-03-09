9278f76f8dd3c1959b9d54358adc1bec0f217b5c
nrs <32944119+nrs-status@users.noreply.github.com>
2026-03-09 20:04:32 +0000

    Update monad_lifts.lean

--- term_ref/monad_lifts.lean
+++ term_ref/monad_lifts.lean
@@ -1,6 +1,21 @@
 -- recall you can find instances with the batteries #instances command, see commands/find_instances.lean
 
+MacroM to TermElabM
+```
 #eval show TermElabM Unit from do
   let r <- liftMacroM <| expandMatchAlts? (<- my_match_stx)
   dbg_trace r
+```
 
+CommandElabM to MetaM
+```
+import Lean
+
+open Lean Parser Elab Meta Term Command
+
+def myMetaM : MetaM Unit := do
+  let addstx : Syntax <- `(def add (n : Nat) (m : Nat) : Nat := Nat.casesOn m n (fun x => add n.succ x))
+  let defview <- liftCommandElabM <| mkDefView {} (addstx.setKind ``Parser.Command.definition)
+  dbg_trace defview.value
+  pure .unit
+```
