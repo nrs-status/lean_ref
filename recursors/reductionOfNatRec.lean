6a8f5c3da4416944072923f713bda8c2406f032a
nrs <32944119+nrs-status@users.noreply.github.com>
2026-03-09 22:03:48 +0000

    Update reductionOfNatRec.lean

--- recursors/reductionOfNatRec.lean
+++ recursors/reductionOfNatRec.lean
@@ -12,3 +12,14 @@ theorem nat_rec_at_succ : @Nat.rec mot at_zero at_succ (Nat.succ nn) = at_succ n
 
 theorem nat_recOn_at_zero : @Nat.recOn mot .zero at_zero at_succ = at_zero := rfl
 theorem nat_recOn_at_succ : @Nat.recOn mot (.succ nn) at_zero at_succ = at_succ nn (@Nat.recOn mot nn at_zero at_succ) := rfl
+
+/--
+info: @[reducible] def Nat.casesOn.{u} : {motive : Nat → Sort u} →
+  (t : Nat) → motive Nat.zero → ((n : Nat) → motive n.succ) → motive t :=
+fun {motive} t zero succ => Nat.rec zero (fun n n_ih => succ n) t
+-/
+#guard_msgs in
+#print Nat.casesOn
+
+theorem nat_casesOn_at_zero : @Nat.casesOn mot .zero at_zero at_succ = at_zero := rfl
+theorem nat_casesOn_at_succ : @Nat.casesOn mot (.succ nn) at_zero at_succ = at_succ nn := rfl
