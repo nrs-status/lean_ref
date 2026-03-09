8f0f5c9eea463dd51207148a3d0061ab16ba36ca
nrs <32944119+nrs-status@users.noreply.github.com>
2026-03-09 16:43:32 +0000

    Update structure_syntax.lean

--- term_ref/structure_syntax.lean
+++ term_ref/structure_syntax.lean
@@ -1,1 +1,4 @@
 {env, options := {}}
+
+def categoryParser (catName : Name) (prec : Nat) : Parser where
+  fn := adaptCacheableContextFn ({ · with prec }) (withCacheFn catName (categoryParserFn catName))
