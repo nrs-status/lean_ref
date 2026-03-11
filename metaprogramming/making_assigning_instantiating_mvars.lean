b342b8660a0ceb6d7bd3fac5c18ebeb42b81ac9b
nrs <32944119+nrs-status@users.noreply.github.com>
2026-03-11 06:01:31 +0000

    Update making_assigning_instantiating_mvars.lean

--- metaprogramming/making_assigning_instantiating_mvars.lean
+++ metaprogramming/making_assigning_instantiating_mvars.lean
@@ -1,8 +1,13 @@
+/--
+?_uniq.8821
+Nat.succ ?_uniq.8821
+Nat.succ (OfNat.ofNat.{0} Nat 42 (instOfNatNat 42))
+-/
 #eval show MetaM Unit from do
-  let mvar1 ← mkFreshExprMVar (mkConst `Nat)
-  let e := (mkConst `Nat.succ).app mvar1
-  -- e is `Nat.succ ?m.773`, `?m.773` is unassigned
+  let mvar1 : Expr <- mkFreshExprMVar (mkConst `Nat)
+  let e : Expr := (Lean.mkConst `Nat.succ).app mvar1
   mvar1.mvarId!.assign (mkNatLit 42)
-  -- e is `Nat.succ ?m.773`, `?m.773` is assigned to `42`
-  let e' ← instantiateMVars e
-  -- e' is `Nat.succ 42`, `?m.773` is assigned to `42`
+  let e' : Expr <- instantiateMVars e
+  dbg_trace mvar1
+  dbg_trace e
+  dbg_trace e'
