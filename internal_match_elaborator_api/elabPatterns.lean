c6b8dafc4217e5cf03f1c684e630283dae06e931
nrs <32944119+nrs-status@users.noreply.github.com>
2026-03-11 04:47:57 +0000

    Update elabPatterns.lean

--- internal_match_elaborator_api/elabPatterns.lean
+++ internal_match_elaborator_api/elabPatterns.lean
@@ -8,31 +8,35 @@ open Lean Elab Term
 
 open private elabMatchAltView withElaboratedLHS elabPatterns from Lean.Elab.Match
 
-def myexpr1 : Expr := .forallE `x q(Nat) q(Nat) .default
-
-def myexpr2 : Expr := .forallE `p q(2 + 2 = 4) q(4 = 4) .default
-
 /--
-(#[
-  [mdata _patWithRef: 
-    ([mdata _patWithRef: Nat.succ]) 
-    ([mdata _patWithRef: ?_uniq.2764])]], Nat)
-(#[
-  [mdata _patWithRef: 
-    ([mdata _patWithRef: Eq.refl.{?_uniq.2765}]) 
-    ?_uniq.2766 
-    ([mdata _patWithRef: ?_uniq.2767])]], 
-  Eq.{1} 
-    Nat 
-    (OfNat.ofNat.{0} Nat 4 (instOfNatNat 4)) 
-    (OfNat.ofNat.{0} Nat 4 (instOfNatNat 4)))
+(#[[mdata _patWithRef: 
+  ([mdata _patWithRef: Nat.succ]) 
+  ([mdata _patWithRef: ?_uniq.3133])]], Nat)
+(#[[mdata _patWithRef: 
+  ([mdata _patWithRef: Eq.refl.{?_uniq.3134}]) 
+  ?_uniq.3135 
+  ([mdata _patWithRef: ?_uniq.3136])]], 
+    Eq.{1} 
+      Nat 
+      (OfNat.ofNat.{0} Nat 4 (instOfNatNat 4)) 
+      (OfNat.ofNat.{0} Nat 4 (instOfNatNat 4)))
+(#[[mdata _patWithRef: 
+  ([mdata _patWithRef: Nat.succ]) 
+  ([mdata _patWithRef: ?_uniq.3137])], 
+    [mdata _patWithRef: 
+      ([mdata _patWithRef: Nat.succ]) 
+      ([mdata _patWithRef: ?_uniq.3138])]], Nat)
 -/
+
 #eval show TermElabM Unit from do
   let mypat <- `(Nat.succ _)
-  let xexcept <- elabPatterns #[mypat] 1 myexpr1
+  let xexcept <- elabPatterns #[mypat] 1 q(Nat -> Nat)
   let r <- ofExcept <| xexcept.mapError PatternElabException.ex
   dbg_trace r
   let mypat' <- `(Eq.refl _)
-  let xexcept' <- elabPatterns #[mypat'] 1 myexpr2
+  let xexcept' <- elabPatterns #[mypat'] 1 q(2 + 2 = 4 -> 4 = 4)
   let r' <- ofExcept <| xexcept'.mapError PatternElabException.ex
   dbg_trace r'
+  let xexcept'' <- elabPatterns #[mypat, mypat] 2 q(Nat -> Nat -> Nat)
+  let r'' <- ofExcept <| xexcept''.mapError PatternElabException.ex
+  dbg_trace r''
