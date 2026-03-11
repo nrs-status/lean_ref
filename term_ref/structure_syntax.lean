839c061499dc59cd9090394d7975c3f6ea4a770a
nrs <32944119+nrs-status@users.noreply.github.com>
2026-03-11 05:53:27 +0000

    Update structure_syntax.lean

--- term_ref/structure_syntax.lean
+++ term_ref/structure_syntax.lean
@@ -1,4 +1,7 @@
 {env, options := {}}
 
+for (fvar, baseId) in altLHS.fvarDecls.toArray.reverse.zip toClear.reverse do
+  pushInfoLeaf <| .ofFVarAliasInfo { id := fvar.fvarId, baseId, userName := fvar.userName }
+
 def categoryParser (catName : Name) (prec : Nat) : Parser where
   fn := adaptCacheableContextFn ({ · with prec }) (withCacheFn catName (categoryParserFn catName))
