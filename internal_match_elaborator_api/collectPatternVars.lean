2a1c34456015838e3e622112f35d044d80c62a40
nrs <32944119+nrs-status@users.noreply.github.com>
2026-03-11 02:59:40 +0000

    Update collectPatternVars.lean

--- internal_match_elaborator_api/collectPatternVars.lean
+++ internal_match_elaborator_api/collectPatternVars.lean
@@ -13,13 +13,19 @@ def mycollecting := show TermElabM _ from
     collectPatternVars (<- myview)
 
 /--
-info: #[
-  `y._@.Macroexpander.garden.4033628405._hygCtx._hyg.2, 
-  `x._@.Macroexpander.garden.4033628405._hygCtx._hyg.2, 
-  `z._@.Macroexpander.garden.4033628405._hygCtx._hyg.2]
+info: #[`y._@.Macroexpander.garden2.3384536090._hygCtx._hyg.2, `x._@.Macroexpander.garden2.3384536090._hygCtx._hyg.2, `z._@.Macroexpander.garden2.3384536090._hygCtx._hyg.2]
+true
+false
+true
+true
 -/
 #guard_msgs in
 #eval show TermElabM Unit from do
   let r <- mycollecting
+  let v <- myview
   dbg_trace r.1
+  dbg_trace r.2.ref == v.ref
+  dbg_trace r.2.patterns == v.patterns
+  dbg_trace r.2.lhs == v.lhs
+  dbg_trace r.2.rhs == v.lhs
 
