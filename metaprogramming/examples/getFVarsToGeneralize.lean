c10f3677dd5252a399de57eb0a191b738b7b9c41
nrs <32944119+nrs-status@users.noreply.github.com>
2026-03-14 04:32:27 +0000

    Update getFVarsToGeneralize.lean

--- metaprogramming/examples/getFVarsToGeneralize.lean
+++ metaprogramming/examples/getFVarsToGeneralize.lean
@@ -6,7 +6,6 @@ open Lean Meta
 
 deriving instance Repr for FVarIdSet
 
-#guard_msgs in
 #eval show MetaM Unit from do
   withLocalDeclDQ `x q(Nat) fun x =>
   withLocalDeclDQ `eq q($x = 4) fun eq => do
@@ -24,4 +23,23 @@ info: Lean.Name.mkNum `_uniq 978
 #[]
 #[]
 -/
+
+#eval show MetaM Unit from do
+  let u : Level := .param `u
+  withLocalDeclDQ `α q(Sort u) fun α =>
+  withLocalDeclDQ `b q(Bool) fun b =>
+  withLocalDeclDQ `ifTrue q($b = true -> $α) fun ifTrue => 
+  withLocalDeclDQ `ifFalse q($b = true -> $α) fun ifFalse => do
+    let x <- getFVarsToGeneralize #[b]
+    dbg_trace repr ifTrue.fvarId!
+    dbg_trace repr ifFalse.fvarId!
+    dbg_trace repr x
+
+/--
+info: Lean.Name.mkNum `_uniq 18982
+Lean.Name.mkNum `_uniq 18983
+#[Lean.Name.mkNum `_uniq 18982, Lean.Name.mkNum `_uniq 18983]
+-/
+
+
 
