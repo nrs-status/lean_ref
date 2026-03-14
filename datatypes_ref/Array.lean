98363006d8cb2274cf669c2e110f82f37cde1ec2
nrs <32944119+nrs-status@users.noreply.github.com>
2026-03-14 05:23:57 +0000

    Create Array.lean

---
+++ datatypes_ref/Array.lean
@@ -0,0 +1,9 @@
+self-described
+```
+empy
+size
+push -- add to end
+pop -- remove from end
+```
+Array.getInternal is used with notation: xarray[i]
+Array.get!Internal is used with notation: xarray[i]!
