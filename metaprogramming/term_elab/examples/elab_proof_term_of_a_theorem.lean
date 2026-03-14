c80ea11d40d320396fdc7d663e607d84cf00b0ad
nrs <32944119+nrs-status@users.noreply.github.com>
2026-03-14 02:14:40 +0000

    Update elab_proof_term_of_a_theorem.lean

--- metaprogramming/term_elab/examples/elab_proof_term_of_a_theorem.lean
+++ metaprogramming/term_elab/examples/elab_proof_term_of_a_theorem.lean
@@ -16,3 +16,20 @@ elab "test" : term => do
 def mythm' : (α : Sort u) -> (x y : α) -> (eq : x = y) -> y = x := 
   test
 
+elab "test'" : term => do
+  let lctx <- getLCtx
+  let αdecl := lctx.getFromUserName! `α
+  let u := αdecl.type.sortLevel!
+  let .some α <- checkTypeQ αdecl.toExpr q(Sort u) | throwError "αerror"
+  let x' := lctx.getFromUserName! `x |>.toExpr
+  let .some x <- checkTypeQ x' q($α) | throwError "error"
+  let y' := lctx.getFromUserName! `y |>.toExpr
+  let .some y <- checkTypeQ y' q($α) |  throwError "error"
+  let eq' := lctx.getFromUserName! `eq |>.toExpr
+  let .some eq <- checkTypeQ eq' q($x = $y) |  throwError "error"
+  let myexpr : Expr := q(@Eq.rec $α $x (fun a_1 _ => a_1 = $x) (Eq.refl $x) $y $eq)
+  .pure myexpr
+
+theorem mythm'' (α : Sort u) (x y : α) (eq : x = y) : y = x :=
+  test'
+
