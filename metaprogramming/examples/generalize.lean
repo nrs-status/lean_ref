72f9a36ac9d6510d3f65ede3cbeb50bf6fa38a5d
nrs <32944119+nrs-status@users.noreply.github.com>
2026-03-14 04:59:06 +0000

    Create generalize.lean

---
+++ metaprogramming/examples/generalize.lean
@@ -0,0 +1,62 @@
+import Lean
+import Qq
+import Batteries.Tactic.OpenPrivate
+
+open private generalize from Lean.Elab.Match
+
+open Qq
+open Lean Meta Elab Term
+
+deriving instance Repr for FVarIdSet
+
+def myview1 : TermElabM TermMatchAltView := do .pure {
+  ref := <- `(`x)
+  patterns := #[<- `(Bool.true)]
+  lhs := <- `(`y)
+  rhs := <- `(ifTrue h) }
+
+def myview2 : TermElabM TermMatchAltView := do .pure {
+  ref := <- `(`x)
+  patterns := #[<- `(Bool.false)]
+  lhs := <- `(`y)
+  rhs := <- `(ifFalse h) }
+
+deriving instance Repr for Discr
+
+#eval show TermElabM Unit from do
+  let u : Level := .param `u
+  withLocalDeclDQ `α q(Sort u) fun α =>
+  withLocalDeclDQ `b q(Bool) fun b =>
+  withLocalDeclDQ `ifTrue q($b = true -> $α) fun ifTrue => 
+  withLocalDeclDQ `ifFalse q($b = true -> $α) fun ifFalse => do
+    let discrs := #[Discr.mk b (.some (.atom .none "h"))]
+    let r <- generalize 
+      discrs
+      q(Bool -> $α) 
+      #[<- myview1, <- myview2]
+      .none
+    dbg_trace repr b.fvarId!
+    dbg_trace repr ifTrue.fvarId!
+    dbg_trace repr ifFalse.fvarId!
+    dbg_trace repr r.discrs
+    dbg_trace repr r.toClear
+    dbg_trace repr r.refined
+    dbg_trace (<- Lean.PrettyPrinter.ppExpr r.matchType)
+    let altviews := r.altViews
+    for view in altviews do
+      dbg_trace view.patterns.map Lean.Syntax.prettyPrint
+
+/--
+info: Lean.Name.mkNum `_uniq 16821
+Lean.Name.mkNum `_uniq 16822
+Lean.Name.mkNum `_uniq 16823
+#[{ expr := Lean.Expr.fvar (Lean.Name.mkNum `_uniq 16821), h? := some (Lean.Syntax.atom (Lean.SourceInfo.none) "h") },
+  { expr := Lean.Expr.fvar (Lean.Name.mkNum `_uniq 16822), h? := none },
+  { expr := Lean.Expr.fvar (Lean.Name.mkNum `_uniq 16823), h? := none }]
+#[Lean.Name.mkNum `_uniq 16822, Lean.Name.mkNum `_uniq 16823]
+true
+(a : Bool) → (a = true → α) → (a = true → α) → α
+#[ Bool.true ,  ifTrue ,  ifFalse ]
+#[ Bool.false ,  ifTrue ,  ifFalse ]
+-/
+
