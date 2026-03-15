25d17255ebf55d51e65fc0e94b89d0bd68c26d21
nrs <32944119+nrs-status@users.noreply.github.com>
2026-03-15 16:56:46 +0000

    Update writer_monad_let_expansion.lean

--- metaprogramming/term_elab/examples/writer_monad_let_expansion.lean
+++ metaprogramming/term_elab/examples/writer_monad_let_expansion.lean
@@ -1,45 +1,50 @@
 import Mathlib.Control.Monad.Writer
+import Batteries.Tactic.OpenPrivate
 import Qq
 
-open Lean Elab Term Meta Syntax
+open Lean Elab Term Meta PrettyPrinter Syntax
 open Qq
 
 inductive LetBindingLHS
 | name (n : Name) | prod_ctor (stx : Syntax) | anon_ctor (stx : Syntax)
+deriving Repr, Inhabited
+
+inductive LetBindingBinding
+| def_ | bind
 deriving Repr
 
 structure LetBinding where
   lhs : LetBindingLHS
-  rhs : String
-  typeName : Lean.Name
+  binding : LetBindingBinding
+  rhs_stx : String
+  typeName : String
   rhs_val : Dynamic
 
 deriving instance TypeName for Nat
 
 instance : Repr LetBinding where
   reprPrec := fun lb n =>
-    reprPrec lb.lhs n ++ " " ++ reprPrec lb.rhs n ++ " " ++ reprPrec lb.typeName n
-
-def myMonad : IO Unit := do
-  let x := 5
-  IO.println x
-  let x' := x + x
-  IO.println x'
+    reprPrec lb.binding n ++ reprPrec lb.binding n ++ reprPrec lb.rhs_stx n
 
 abbrev MyWriter := WriterT (Array LetBinding)
-
-def myNewMonad : MyWriter IO Unit := do
-  let x : Nat := 5
-  tell #[.mk (.name `x) "5" `Nat (.mk x)]
-  IO.println x
-  let x' := x + x
-  tell #[.mk (.name `x') "x + x" `Nat (.mk x')]
-  IO.println x'
 
 
 syntax (name := let'_stx) "let' " ident (" : " term)? " := " term : doElem
 
+
 set_option pp.notation false
+def mkTellTerm (id_nm : Lean.Name) (rhs_stx : Syntax) : TermElabM Expr := do
+  let lhs_ctor : Q(LetBindingLHS) := q(LetBindingLHS.name $id_nm)
+  let rhs_str_expr : Q(String) := mkStrLit (<- formatTerm rhs_stx).pretty'
+  let lctx <- getLCtx
+  let decl := lctx.getFromUserName! id_nm
+  let ty_as_str : String := (<- Meta.ppExpr decl.type).pretty'
+  let decl_ty_nm : Q(String) := mkStrLit ty_as_str
+  let dynval_expr : Q(Dynamic) <- mkAppM ``Dynamic.mk #[decl.value]
+  let expr := q(MonadWriter.tell (ω := Array LetBinding) (M := MyWriter IO) #[.mk $lhs_ctor .def_ $rhs_str_expr $decl_ty_nm $dynval_expr])
+  .pure expr
+
+
 @[doElem_elab let'_stx]
 def let'_elab : Do.DoElab := 
   fun stx do_cont => 
@@ -49,25 +54,14 @@ def let'_elab : Do.DoElab :=
       | .some ty' => `(letDecl|$id : $ty' := $body)
       | .none => `(letDecl|$id := $body))
     let id_as_nm := Lean.TSyntax.getId id
-    let nm_stx : TSyntax `term := mkStrLit id_as_nm.toString
-    let nm_ctor_stx <- `(term|LetBindingLHS.name (Name.mkSimple $nm_stx))
     let nm <- mkFreshUserName `___x
     Do.elabDoLetOrReassign (.let .none) let_stx {
       resultName := nm
       resultType := q(Unit)
       k := do
-        let lctx <- getLCtx
-        let declx := lctx.getFromUserName! id_as_nm
-        let expr <- mkAppOptM ``MonadWriter.tell 
-          #[.none, q(MyWriter IO), .none, <- mkAppM ``Array.mkArray1 #[<- mkAppM ``LetBinding.mk #[
-            (<- elabTerm nm_ctor_stx (.some q(LetBindingLHS))),
-            Lean.mkStrLit (<- PrettyPrinter.ppExpr declx.value).pretty',
-            <- mkAppM ``Name.mkSimple #[Lean.mkStrLit (<- PrettyPrinter.ppExpr declx.type).pretty'],
-            (<- mkAppM ``Dynamic.mk #[declx.value])
-          ]]]
-        do_cont.mkBindUnlessPure expr
+        let e <- mkTellTerm id_as_nm body
+        do_cont.mkBindUnlessPure e
         }
-
   | _ => throwUnsupportedSyntax
 
 set_option backward.do.legacy false
@@ -80,10 +74,12 @@ def myLoggedTest' : MyWriter IO Unit := do
 /--
 info: hi
 10
-#[LetBindingLHS.name `x "5" `Nat, LetBindingLHS.name `y "HAdd.hAdd x x" `Nat]
+#[LetBindingBinding.def_LetBindingBinding.def_"5", LetBindingBinding.def_LetBindingBinding.def_"x + x"]
 -/
 #guard_msgs in
 #eval show IO Unit from do
   let r <- myLoggedTest'.run
   dbg_trace repr r.2
   .pure .unit
+
+
