180c6cc4b509e5daeac2e0a6af09fd30a2284af2
nrs <32944119+nrs-status@users.noreply.github.com>
2026-03-27 15:23:01 +0000

    Update Array.lean

--- datatypes_ref/Array.lean
+++ datatypes_ref/Array.lean
@@ -10,7 +10,7 @@ take -- take first i elms
 drop -- drop first i elms
 modify -- change elm at given i
 eraseIdx -- erase elm at i
-zipIdx -- enumerate
+zipIdx -- enumerate, i.e. zip with range
 
 Array.getInternal is used with notation: xarray[i]
 Array.get!Internal is used with notation: xarray[i]!
