3436fa7c3af0814dcbca01f8d880cc4826057cc8
nrs <32944119+nrs-status@users.noreply.github.com>
2026-03-28 00:22:38 +0000

    Update Array.lean

--- datatypes_ref/Array.lean
+++ datatypes_ref/Array.lean
@@ -3,6 +3,7 @@ self-described
 empty
 size
 isPrefixOf
+range
 ```
 push -- add to end
 pop -- remove from end
@@ -12,6 +13,8 @@ drop -- drop first i elms
 modify -- change elm at given i
 eraseIdx -- erase elm at i
 zipIdx -- enumerate, i.e. zip with range
+range' -- range but with start/stop
+replicate -- fill an array with a given value; repeat an identical value; repeat the same value
 
 Array.getInternal is used with notation: xarray[i]
 Array.get!Internal is used with notation: xarray[i]!
