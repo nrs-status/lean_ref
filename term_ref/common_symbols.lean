ebf1da0b3f0d329ecb310312dbba19bdd605bb14
nrs <32944119+nrs-status@users.noreply.github.com>
2026-04-14 05:39:54 +0000

    Update common_symbols.lean

--- term_ref/common_symbols.lean
+++ term_ref/common_symbols.lean
@@ -6,5 +6,12 @@ french quotes identifier quotes
 unaccessible inaccessible hygiene hidden
 ✝ \textdied
 
+get first matching term
 ‹ \f<
 › \f>
+
+CategoryStruct.comp
+≫ \gg
+
+CategoryStruct.id
+𝟙 \b1
